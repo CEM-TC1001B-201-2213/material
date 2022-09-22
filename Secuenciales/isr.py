1e1bfd8730fa8a98a8ef7a5a821836ee1e478c5c
aram-gonzalez-tec <aram.gonzalez@tec.mx>
2022-09-22 10:00:50 -0500

    Secuenciales

---
+++ Secuenciales/isr.py
@@ -0,0 +1,12 @@
+# Actividad 1
+# Ejercicio 1
+# Nombre Matrícula
+# Nombre Matrícula
+# Nombre Matrícula
+# Nombre Matrícula
+
+salario = float(input("Introduce tu salario: "))
+isr = salario * 0.2136
+salario_final = salario - isr
+print("El ISR descontado es: ${isr:,.2f}")
+print("Tu salario después de descontar el ISR es de: ${salario_final:,.2f}")
